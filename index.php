241459e07d176942e857ef6549dcf58f6ad28929
JameseNunCuz <jamese.nuncuz.1@educa.jcyl.es>
2025-10-24 09:27:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James Edward Nuñez Cuzcano</title>
+    <link rel="stylesheet" href="./webroot/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Sostenibilidad Aplicada al Sistema Productivo</h1>
+        </div>
+        <button class="home active" onclick="location.href = '../'"><img src="./webresources/home.png"></button>
+    </div>
+
+    <div class="center-container">
+        <button onclick="window.open('./doc/Caso Practico 1.pdf')" class="active">
+            <p>UD1 Actividad 1 - Caso practico 1</p>
+        </button>
+        <button onclick="window.open('./doc/UD1 Actividad 2 - Limites planetarios.pdf')" class="active">
+            <p>UD1 Actividad 2 - Limites Planetarios</p>
+        </button>
+    </div> 
+
+    <div class="footer">
+        <h3>James Edward Nuñez Cuzcano</h3>
+    </div>
+</body>
+
+</html>
